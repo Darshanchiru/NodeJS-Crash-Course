2ab9e96d69aa255f52187e09f9efc66dee9d15ad
Darshan A <darshan.a@microfocus.com>
2019-08-10 20:47:30 +0530

    Path UtiliTIES Covered

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -1,4 +1,18 @@
 const path=require('path');
 
+console.log(__filename);
 //Base filename
 console.log(path.basename(__filename));
+
+//Direcroty name
+console.log(path.dirname(__filename));
+
+//Filename extension
+console.log(path.extname(__filename));
+
+//Create path Object
+console.log(path.parse(__filename));
+
+
+//Concatenate Path
+console.log(path.join(__dirname,"test","hello.html"))
