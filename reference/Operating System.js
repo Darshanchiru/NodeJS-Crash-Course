53af62330e107bddd903244cdd4c9209110ca517
Darshan A <darshan.a@microfocus.com>
2019-08-11 14:30:04 +0530

    OS Utilities

---
+++ reference/Operating System.js
@@ -0,0 +1,20 @@
+const os=require("os");
+
+//Show platform
+console.log(os.platform());
+
+
+//CPU Arch
+console.log(os.arch());
+
+// CPU Core
+console.log(os.cpus());
+
+// free Memory
+console.log(os.freemem());
+
+//Total Memory
+console.log(os.totalmem());
+
+// Home dir
+console.log(os.homedir());
