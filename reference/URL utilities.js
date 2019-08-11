851d701f255c1c1d72bcb47437c16f82839e19a7
Darshan A <darshan.a@microfocus.com>
2019-08-11 19:07:17 +0530

    URL utilities

--- reference/URL utilities.js
+++ reference/URL utilities.js
@@ -1,2 +1,31 @@
 const url=require('url');
 
+const myUrl=new URL("https://www.mywebsite.com/hello.html?id=21&status=active");
+
+console.log(myUrl);
+
+// Serialized
+console.log(myUrl.href);
+console.log(myUrl.toString());
+
+// HOST root domain]
+console.log(myUrl.host);
+
+// Hostname
+console.log(myUrl.hostname);
+
+// Path name
+console.log(myUrl.pathname);
+
+// Serialized Query
+console.log(myUrl.search);
+
+// Param Object
+console.log(myUrl.searchParams);
+
+// Adding params
+myUrl.searchParams.append('abc','darshan');
+console.log(myUrl.searchParams);
+
+// Loop through Params
+myUrl.searchParams.forEach((value,name)=>console.log(`${name}:${value}`));
